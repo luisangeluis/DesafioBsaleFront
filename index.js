9ec00343ba10d59b879dc58e797e34c8054c436b
luisangeluis <luisangeluis@gmail.com>
2022-10-16 22:24:33 -0500

    Edited readme

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 import { getProducts, getProductsByCategoryId, getProductsByTerm } from "./products/index.products.js";
-import { reviewTerm } from './products/ui.products.js';
 import { getCategories } from './categories/index.categories.js';
-import { toggleCategories } from './categories/ui.categories.js';
 
 getCategories();
 getProducts();
-
-// toggleCategories();
-
 
 window.getProducts = getProducts;
 window.getProductsByCategoryId = getProductsByCategoryId;
